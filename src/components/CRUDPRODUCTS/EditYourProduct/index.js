33ed57e468a713215ae426f9318d8c2e531a4569
Nguyen Quoc Duan <nguyenquocduanpk9@gmail.com>
2021-08-26 10:25:51 +0700

    temprary

--- src/components/CRUDPRODUCTS/EditYourProduct/index.js
+++ src/components/CRUDPRODUCTS/EditYourProduct/index.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import { fetchProduct, editProduct } from '../../../redux/Product/product.actions';
+import FormAnt from '../FormAnt';
 import ProductForm from '../ProductForm';
 
 
@@ -11,28 +12,40 @@ const EditYourProduct = (props) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const params = useParams();
-  console.log("params", params);
+  const mapState = (state) => ({
+    product: state.products[params.id]
+  })
+  const { product } = useSelector(mapState);
 
   useEffect(() => {
     dispatch(fetchProduct(params.id));
-  }, [])
+  }, []);
 
-  const onSubmit = async (formValues) => {
-    await dispatch(editProduct(props.match.params.id, formValues));
+  /* const onSubmit = async (formValues) => {
+    console.log(formValues)
+    await dispatch(editProduct(params.id, formValues));
     history.push('/products');
+  } */
+
+  const onFinish = async (values) => {
+    console.log(values)
+    await dispatch(editProduct(params.id, values));
+    //history.push('/products');
   }
-  if (!props.product) {
+
+  if (!product) {
     return (
       <div>
         ...Loading
       </div>
     )
-  }
-  return (
-    <ProductForm
-      onSubmit={onSubmit}
-      initialValues={_.pick(props.product, 'title', 'price', 'image', 'description')} />
-  )
+  } else
+    return (
+      /*  <ProductForm
+         onSubmit={onSubmit}
+         initialValues={_.pick(product, 'title', 'price', 'image', 'description')} /> */
+      <FormAnt onFinish={onFinish} />
+    )
 }
 
 export default EditYourProduct;
